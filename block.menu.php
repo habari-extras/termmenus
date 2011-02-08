f9d88ce1e8c785a6a68d878885e20b72213dc65c
mikelietz <codger+git@gmail.com>
2011-02-08 02:31:00 +0000

    plugin:termmenus Show titles of links in the block template. With a callback function we should be able to link them to URLs, too.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/termmenus/trunk@3690 f4a90942-1786-4aa1-b092-57c31935ddc3

--- block.menu.php
+++ block.menu.php
@@ -1,1 +1,4 @@
-<?php Utils::debug( $content->vocabulary ); ?>
+<?php /*Utils::debug( $content->vocabulary ); */
+$v = $content->vocabulary;
+echo Format::term_tree( $v->get_tree(), $v->name, '%s', '<ol style="#menu">', '</ol>');
+?>
